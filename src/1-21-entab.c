913bb40dca88a0910ddcd8f0bfd1a7bfa0661887
Will Demilio <wdemilio@gmail.com>
2017-01-11 11:44:53 -0500

    Working on 1-21

--- src/1-21-entab.c
+++ src/1-21-entab.c
@@ -51,6 +51,7 @@ void retrieve(char target[], int limit);
 void parse(char source[], char target[]);
 void retab(char source[], char target[]);
 
+/* Main control program */
 int main(void)
 {
 	char line[MAXLINE], output[MAXLINE];
@@ -66,7 +67,7 @@ int main(void)
 	return 0;
 }
 
-
+/* Loads a line */
 void retrieve(char s[], int lim)
 {
     int c, i;
@@ -79,6 +80,7 @@ void retrieve(char s[], int lim)
     s[i] = '\0'; 
 }
 
+/* Strips out existing stray tabs */
 void parse(char s[], char t[])
 {
 	int i, j, o, r;
@@ -99,28 +101,48 @@ void parse(char s[], char t[])
 	t[o] = '\0';
 }
 
-void retab(char s[], char t[])
+void retab(char input[], char output[])
 {
 	/*
 		overall length is going to be getting SHORTER here so...
-		i = num chars after entab
-		o = original character length
-		Every time you insert a tab you need to make sure to keep incrementing o
+		newCharLength = num chars after entab
+		oldCharLength = original character length
+		charsToTabStop = starts at 8, ends at 1
+		contiguousSpaces = counter for number of spaces in a sequence
+
+		Every time you insert a tab you need to make sure to keep incrementing oldCharLength
 	*/
-	int i, j, o, r;
-	o = 0;
-	for(i=0; i < MAXLINE && s[i] != '\0'; ++i){
-		if(s[i] == '\t'){
-			r = (o % TABSTOP);
-			for(j=0; j < TABSTOP - r; ++j){
-				t[o] = ' ';
-				o++;
-			}
+	int newCharLength, oldCharLength, charsToTabStop, contiguousSpaces, iterateThroughSpaces;
+	newCharLength = 0;
+	charsToTabStop = 0;
+	contiguousSpaces = 0;
+	/* iterate through original string input */
+	for(oldCharLength=0; oldCharLength < MAXLINE && input[oldCharLength] != '\0'; ++oldCharLength){
+		if(input[oldCharLength] != ' '){
+			output[newCharLength] = input[oldCharLength];
+			++newCharLength;
 		}
 		else {
-			t[o] = s[i];
-			o++;
+			/* Start looking for spaces */
+			for(iterateThroughSpaces = oldCharLength; 
+				iterateThroughSpaces < MAXLINE && input[iterateThroughSpaces] == ' ';
+				++iterateThroughSpaces){
+				++contiguousSpaces;
+			}
+			/* Handle Multiple Spaces */
+			if (contiguousSpaces > 0){
+				charsToTabStop = TABSTOP - (oldCharLength % TABSTOP);
+				if(charsToTabStop <= contiguousSpaces){
+					output[newCharLength] = '\t';
+					++newCharLength;
+					oldCharLength += charsToTabStop;
+				}
+			}
+			/* Handle Single Spaces */
+			else {
+				output[newCharLength] = input[oldCharLength];
+				++newCharLength;
+			}
 		}
 	}
-	t[o] = '\0';
 }
