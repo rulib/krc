ef0a03991c975551c108546d78638454a97e0427
Will <wdemilio@gmail.com>
2017-01-12 08:23:47 -0500

    1-22-fold.c meets spec, some edge cases (terminal space exactly at line break point) not accounted for

--- src/1-22-fold.c
+++ src/1-22-fold.c
@@ -13,7 +13,10 @@ void printFolded(char source[]);
 int main(void)
 {
 	char line[MAXLINE];
-	while(line[0] != '\0'){
+	while(1){
+		if(line[0] == '\0'){
+			break;
+		}
 		retrieve(line, MAXLINE);
 		printFolded(line);
 	}
@@ -36,35 +39,71 @@ void retrieve(char s[], int lim)
 /* 
 breaks lines > MAXOUT into multiple lines at last space before MAXOUT chars
 If no spaces, breaks at MAXOUT chars 
+
+output is string to be printed
+next is string to be recursed on
 */
-void printFolded(char line)
+void printFolded(char line[])
 {
-	int charCounter, lastSpace, breakCounter;
-	char output[MAXLINE];
-	charCounter = lastspace = 0;
+	int charCounter, breakPoint, breakCounter, forced;
+	char output[MAXLINE], next[MAXLINE];
+	forced = 0;
+	/* Sets up 'next' to not recurse if unused*/
+	next[0] = '\0';
+	charCounter = breakPoint = breakCounter = 0;
+	/* Counts and checks for breakpoint */
 	while(charCounter < MAXLINE && line[charCounter] != '\n' && line[charCounter] != '\0'){
-		/*
-		Check for breakpoints
-		*/
+		/* Check for breakpoints */
 		if(charCounter < MAXOUT && line[charCounter] == ' '){
-			lastSpace = charCounter;
+			breakPoint = charCounter;
 		}
+		charCounter++;
 	}
+	/* Handle short lines and move on */
 	if(charCounter <= MAXOUT) {
+		line[charCounter+1] = '\0';
 		printf("%s", line);
+		printf("EOS");
 	}
-	else if (charCounter > MAXOUT && lastSpace > 0){
-		breakCounter = 0;
-		while(breakCounter < lastSpace){
+	/* Split at last space before 80 or 80 char */
+	else if (charCounter > MAXOUT){
+		/* puts first 80 into output */
+		if(breakPoint == 0){
+			breakPoint = MAXOUT;
+			forced = 1;
+		}
+		while(breakCounter < breakPoint && line[breakCounter] != '\0'){
 			output[breakCounter] = line[breakCounter];
-			breakCounter++
+			breakCounter++;
 		}
 		output[breakCounter] = '\n';
-		output[breakCounter+1] = '\0';
+		output[breakCounter + 1] = '\0';
 		printf("%s", output);
-		while(breakCounter < MAXLINE && line[breakCounter] != '\0'){
-			output[breakCounter] = line[breakCounter];
-			breakCounter++
+		/* Skip the space if actually broken at space*/
+		if(forced == 0){
+			breakCounter++;
 		}
+		/* puts remainder into next */
+		while(line[breakCounter] != '\0'){
+			/* Note offset to account for skipping space */
+			if(forced==0){
+				next[breakCounter - (breakPoint+1)] = line[breakCounter];
+			}
+			else{
+				next[breakCounter - (breakPoint)] = line[breakCounter];
+			}
+			breakCounter++;
+		}
+		next[breakCounter] = '\n';
+		next[breakCounter + 1] = '\0';
+		printf("EOO");
+	}
+	/* Handle lines without spaces */
+	/*else if (charCounter > MAXOUT && breakPoint == 0){
+		printf("Get some spaces bitch\n");
+	}*/
+	/* If there is any remainder... */
+	if(next[0] != '\0'){
+		printFolded(next);
 	}
 }
