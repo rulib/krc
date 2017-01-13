3b65af3a4f2c492323af27c15fbf6db050515d21
Will Demilio <wdemilio@gmail.com>
2017-01-13 08:01:40 -0500

    Starting work on syntax checker

--- src/1-24-syntax.c
+++ src/1-24-syntax.c
@@ -14,3 +14,44 @@ that you are not actually in, throw an error.
 Track modes in a big array I guess since we don't have any better data structures yet
 Increment and decrement the index in an integer
 */
+
+#include <stdio.h>
+#define MAXLINE 1024 /* maximum input line size */
+#define MAXSTACK 1024 /* maximum stack size */
+#define MAXOUT 80 /* terminal width - aka max output line */
+#define DEFAULT 0
+#define CURLYBLOCK 1
+#define BRACKETS 2
+#define PAREN 3
+#define SQUOTE 4
+#define DQUOTE 5
+#define COMMENT 6
+#define SINGLECOMMENT 7
+#define OPENERS ('{' || '(' || '[')
+#define CLOSERS ('}' || ')' || ']')
+
+void retrieve(char target[], int limit);
+void parseCode(char target[]);
+
+/* Main control program */
+int main(void)
+{
+	int stackPosition;
+	int stack[MAXSTACK];
+	char line[MAXLINE];
+	while(1){
+		if(line[0] == '\0'){
+			break;
+		}
+		retrieve(line, MAXLINE);
+		printFolded(line);
+	}
+	return 0;
+}
+
+/* Loads a line */
+void parsecode(char s[], int stack[], int stackPosition)
+{
+    int c, i;
+}
+
